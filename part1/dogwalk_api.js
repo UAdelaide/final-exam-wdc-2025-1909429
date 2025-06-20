45337622d170aefe0075879d22257ba7a1c17aa9
a1909429 <a1909429@adelaide.edu.au>
2025-06-20 09:17:22 +0000

    [automatic commit] 2025-06-20 09:17:22 UTC 119.15.74.56

--- part1/dogwalk_api.js
+++ part1/dogwalk_api.js
@@ -6,4 +6,10 @@ const PORT = 8080;
 let db;
 async function initializeDatabase() {
   db = await mysql.createConnection({
-    host: 'localhost
+    host: 'localhost',
+    user:'root',
+    password: '',
+    database: 'dogwalk'
+  });
+  console.log('Database connected');
+}
