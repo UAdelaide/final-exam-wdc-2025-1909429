2225b8542d7d9f8eb806abb36b3bd8378300af15
a1909429 <a1909429@adelaide.edu.au>
2025-06-20 09:22:40 +0000

    [automatic commit] 2025-06-20 09:22:39 UTC 119.15.74.56

--- part1/dogwalk_api.js
+++ part1/dogwalk_api.js
@@ -16,9 +16,9 @@ async function initializeDatabase() {
 
 app.get('/api/walks', async (req, res) => {
   try {
-    const [rows] = await db.execute('
+    const [rows] = await db.execute(`
         SELECT Dogs.name AS dog_name,Dogs.size,Users.name AS owner_username FROM Dogs
-        JOIN Users ON Dogs.owner_id = Users.user_id');
+        JOIN Users ON Dogs.owner_id = Users.user_id`);
     res.json(rows);
   }catch (error) {
     res.status(500).json({ error: 'Database query failed' ,details: error.message  });
