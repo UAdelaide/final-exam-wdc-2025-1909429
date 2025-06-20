8b13efdf037ca0d1209a74ed1caaab757fe4329e
a1909429 <a1909429@adelaide.edu.au>
2025-06-20 16:09:48 +0000

    [automatic commit] 2025-06-20 16:09:48 UTC 119.15.74.56

--- part2/routes/dogrouters.js
+++ part2/routes/dogrouters.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
+
+router.get('/dogs', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+      FROM Dogs
+      LEFT JOIN Users ON Dogs.owner_id = Users.user_id
+    `);
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
