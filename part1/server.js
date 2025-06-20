54730ea1a369a1e02c3ee7cdf75e46f9d1b8474a
a1909429 <a1909429@adelaide.edu.au>
2025-06-20 10:38:52 +0000

    [automatic commit] 2025-06-20 10:38:52 UTC 119.15.74.56

--- part1/server.js
+++ part1/server.js
@@ -8,5 +8,6 @@ app.use('/api', dogwalkApi);
 
 const PORT = 8080;
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`API server running at http://localhost:${PORT}`);
 });
