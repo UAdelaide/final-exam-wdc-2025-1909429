23800d521f153eb19e628fcccccb7ff0b30d78d8
a1909429 <a1909429@adelaide.edu.au>
2025-06-20 09:26:10 +0000

    [automatic commit] 2025-06-20 09:26:10 UTC 119.15.74.56

--- part1/dogwalk_api.js
+++ part1/dogwalk_api.js
@@ -1,6 +1,6 @@
-const express= require('express');
+const express = require('express');
+const router = express.Router();
 const mysql = require('mysql2/promise');
-const app = express();
 const PORT = 8080;
 
 let db;
