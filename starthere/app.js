ab85162744a763ea32646dcc13a2d784636336d7
a1909429 <a1909429@adelaide.edu.au>
2025-06-20 10:10:22 +0000

    [automatic commit] 2025-06-20 10:10:22 UTC 119.15.74.56

--- starthere/app.js
+++ starthere/app.js
@@ -6,13 +6,13 @@ var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
 var app = express();
-
+app.use('/api', dogwalkApi);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/api', dogwalkApi);
+
 let db;
 
 (async () => {
